acc0e95b05ace4e03c7251c33f79087782d3e6fc
Kade Griffith <kadeg95@gmail.com>
2021-08-13 09:34:04 -0600

    successfully requested and recieved data from swapi. going to look at the data and decide what components to build

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import BASE_URL from './constants';
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [data, setData] = useState(null);
+  const [character, setCharacter] = useState('');
+  const [error, setError] = useState(null);
+
+  useEffect( () => {
+    axios.get(BASE_URL)
+      .then( res => setData(res.data))
+      .catch( err => setError(err))
+  }, [])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
+    data && <div className="App">
+      {error && <h1>Oops, something unexpected happened! {error}</h1>}
       <h1 className="Header">Characters</h1>
     </div>
   );
